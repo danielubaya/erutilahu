51f293a366a894f3749bc027d4aee28fb56354f7
daniel <daniel@staff.ubaya.ac.id>
2022-05-09 14:48:40 +0700

    alurOK

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+//URL::forceRootUrl('https://erutilahu.cktr.web.id/');
 Auth::routes();
+
+// Login Routes...
+Route::get('admin-login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+Route::post('admin-login', 'Admin\LoginController@login');
+
+// Logout Routes...
+Route::post('admin-logout', 'Admin\LoginController@logout')->name('admin.logout');
+
+// Registration Routes...
+Route::get('admin-register', 'Admin\RegisterController@showRegistrationForm')->name('admin.register');
+Route::post('admin-register', 'Admin\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('admin-password/reset', 'Admin\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+Route::post('admin-password/email', 'Admin\ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+Route::get('admin-password/reset/{token}', 'Admin\ResetPasswordController@showResetForm')->name('admin.password.reset');
+Route::post('admin-password/reset', 'Admin\ResetPasswordController@reset')->name('admin.password.update');
+
+// Password Confirmation Routes...
+Route::get('admin-password/confirm', 'Admin\ConfirmPasswordController@showConfirmForm')->name('admin.password.confirm');
+Route::post('admin-password/confirm', 'Admin\ConfirmPasswordController@confirm');
+
+// Email Verification Routes...
+Route::get('admin-email/verify', 'Admin\VerificationController@show')->name('admin.verification.notice');
+Route::get('admin-email/verify/{id}/{hash}', 'Admin\VerificationController@verify')->name('admin.verification.verify');
+Route::post('admin-email/resend', 'Auth\VerificationController@resend')->name('admin.verification.resend');
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
@@ -24,7 +50,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('usulan_baru', [App\Http\Controllers\UsulanController::class, 'usulan_baru']);	
     Route::post('tambah_usulan', [App\Http\Controllers\UsulanController::class, 'tambah_usulan']);	
     Route::post('kirim_ke_dinas', [App\Http\Controllers\UsulanController::class, 'kirim_ke_dinas']);	
-   
+    Route::post('update_nosurat', [App\Http\Controllers\UsulanController::class, 'update_nosurat']);	
+    
     Route::get('show_xy/{id}', [App\Http\Controllers\UsulanController::class, 'show_xy']);	
     Route::get('isi_modal_setxy/{id}', [App\Http\Controllers\UsulanController::class, 'isi_modal_setxy']);	
     Route::post('update_xy', [App\Http\Controllers\UsulanController::class, 'update_xy']);	
@@ -39,15 +66,34 @@ Route::middleware(['auth'])->group(function () {
     Route::post('hapus_file', [App\Http\Controllers\UsulanController::class, 'hapus_file']);	
     Route::get('list_dokumen/{id}/{target}', [App\Http\Controllers\UsulanController::class, 'list_dokumen']);	
     
-    Route::get('usulan_masuk', [App\Http\Controllers\UsulanController::class, 'usulan_masuk']);	
+    Route::post('upload_file_ba', [App\Http\Controllers\UsulanController::class, 'upload_file_ba']);	
+    Route::get('daftar_file_ba/{folder}/{target}', [App\Http\Controllers\UsulanController::class, 'daftar_file_ba']);	
+    Route::post('hapus_file_ba', [App\Http\Controllers\UsulanController::class, 'hapus_file_ba']);	
+    
+    Route::get('usulan_masuk/{kelurahan}', [App\Http\Controllers\UsulanController::class, 'usulan_masuk']);	
+    Route::post('kirim_ke_kelurahan', [App\Http\Controllers\UsulanController::class, 'kirim_ke_kelurahan']);	
+    Route::post('kirim_ke_kotak', [App\Http\Controllers\UsulanController::class, 'kirim_ke_kotak']);	
+    Route::post('kirim_ke_ranking', [App\Http\Controllers\UsulanController::class, 'kirim_ke_ranking']);	
+    Route::post('update_rank', [App\Http\Controllers\UsulanController::class, 'update_rank']);	
+    Route::post('kirim_rank', [App\Http\Controllers\UsulanController::class, 'kirim_rank']);	
     
     Route::get('usulan_dicek', [App\Http\Controllers\UsulanController::class, 'usulan_dicek']);	
     Route::get('usulan_ranking', [App\Http\Controllers\UsulanController::class, 'usulan_ranking']);	
+    Route::get('usulan_ranking_ro/{kelurahan}', [App\Http\Controllers\UsulanController::class, 'usulan_ranking_ro']);	
     
-    Route::get('filter_usulan', [App\Http\Controllers\UsulanController::class, 'filter_usulan']);	
+    Route::get('filter_usulan/{tahap}', [App\Http\Controllers\UsulanController::class, 'filter_usulan']);	
     Route::get('usulan_kelurahan/{kelurahan}', [App\Http\Controllers\UsulanController::class, 'usulan_kelurahan']);	
     
-
+    Route::get('usulan_nonrutil/{kelurahan}', [App\Http\Controllers\UsulanController::class, 'usulan_nonrutil']);	
+    
+    Route::get('usulan_daftartunggu/{kelurahan}', [App\Http\Controllers\UsulanController::class, 'usulan_daftartunggu']);	
+    Route::post('mulai_pengerjaan', [App\Http\Controllers\UsulanController::class, 'mulai_pengerjaan']);	
+    
+    Route::get('usulan_pengerjaan/{kelurahan}', [App\Http\Controllers\UsulanController::class, 'usulan_pengerjaan']);	
+    Route::post('selesai_pengerjaan', [App\Http\Controllers\UsulanController::class, 'selesai_pengerjaan']);	
+    
+    Route::get('usulan_selesai/{kelurahan}', [App\Http\Controllers\UsulanController::class, 'usulan_selesai']);	
+    
     Route::get('peta_usulan', [App\Http\Controllers\UsulanController::class, 'peta_usulan']);	
    
     Route::get('logout', [App\Http\Controllers\UsulanController::class, 'logout']);	
